dee7c1c880a72323f0ad05bce2cb697fbe448004
Tiegow <88515883+Tiegow@users.noreply.github.com>
2025-11-02 19:37:34 -0300

    Inserindo falhas nas requisições 1 e 3

--- AirlinesHub/src/main/java/com/airlineshub/AirlinesHub/controller/AirlinesHubController.java
+++ AirlinesHub/src/main/java/com/airlineshub/AirlinesHub/controller/AirlinesHubController.java
@@ -3,6 +3,8 @@ package com.airlineshub.AirlinesHub.controller;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,23 +24,58 @@ public class AirlinesHubController {
     @Autowired
     private FlightRepository flightRepository;
 
+    private AtomicBoolean isSlow = new AtomicBoolean(false);
+    private AtomicLong slowStateEndTime = new AtomicLong();
+
     @GetMapping("/flight")
     public ResponseEntity<Voo> consultarVoo(@RequestParam String flight, @RequestParam LocalDate day) {
-        Voo voo = flightRepository.findVoo(flight, day);
 
+        // -- FALHA (Omissão, 0.2%, 0s) -- \\
+        if (Math.random() < 0.2) {
+            return ResponseEntity.ok(null);
+        }
+
+        // -- EXECUÇÃO NORMAL -- \\
+        Voo voo = flightRepository.findVoo(flight, day);
         return ResponseEntity.ok(voo);
     } 
 
     @PostMapping("/sell")
     public ResponseEntity<String> venderVoo(@RequestParam String flight, @RequestParam LocalDate day) {
-        Voo voo = flightRepository.findVoo(flight, day);
+        
+        long currTime = System.currentTimeMillis();
+        
+        // -- FALHA (Tempo = 5s, 0.1%, 10s) -- \\
+        if (isSlow.get()) {
+            if (currTime < slowStateEndTime.get()) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                isSlow.set(false);
+            }
+        }
 
+        if (Math.random() < 0.1 && isSlow.get() == false) { // 10% de chance de entrar no estado de lentidão
+            isSlow.set(true);
+            slowStateEndTime.set(currTime + 10000);
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }            
+        }
+        
+        // -- EXECUÇÃO NORMAL -- \\
+        Voo voo = flightRepository.findVoo(flight, day);
         if (voo != null) {
             String idTransacao = UUID.randomUUID().toString();
 
             return ResponseEntity.ok(idTransacao);
         }
-        
         return ResponseEntity.notFound().build();
     }   
 
