9c2b16c00d43dc031f702383e8391753d3926f8e
Tiegow <88515883+Tiegow@users.noreply.github.com>
2025-11-07 18:33:08 -0300

    Inserindo erro da requisição 2

--- ExchangeService/src/main/java/com/exchange/ExchangeService/controller/ExchangeController.java
+++ ExchangeService/src/main/java/com/exchange/ExchangeService/controller/ExchangeController.java
@@ -6,12 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.exchange.ExchangeService.service.FailService;
+
 @RestController
 @RequestMapping("/exchange")
 public class ExchangeController {
 
+    private final FailService failService;
+
+    public ExchangeController(FailService failService) {
+        this.failService = failService;
+    }
+
     @GetMapping("/convert")
     public double obterTaxaDolar() {
+
+        failService.createChanceFailError(0.1, 5);
+
         double taxa = ThreadLocalRandom.current().nextDouble(5, 6);
 
         return taxa;
