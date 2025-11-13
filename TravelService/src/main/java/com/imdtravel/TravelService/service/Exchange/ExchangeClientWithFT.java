55ef8f872c2ac14699c109a1fa6899c8d040423c
Tiegow <88515883+Tiegow@users.noreply.github.com>
2025-11-13 18:26:48 -0300

    Implementada tolerância para ExchangeClient

--- TravelService/src/main/java/com/imdtravel/TravelService/service/Exchange/ExchangeClientWithFT.java
+++ TravelService/src/main/java/com/imdtravel/TravelService/service/Exchange/ExchangeClientWithFT.java
@@ -1,12 +1,55 @@
 package com.imdtravel.TravelService.service.Exchange;
 
+import com.imdtravel.TravelService.exception.ExternalServiceException;
 import com.imdtravel.TravelService.model.ExchangeClient;
+
+import java.util.LinkedList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ExchangeClientWithFT implements ExchangeClient {
-    @Override
-    public Double getExchangeRate() {
-        return 0.0;
+
+    private final RestTemplate restTemplate;
+    
+    private final String exchangeServiceUrl;
+
+    // FT:
+    private LinkedList<Double> ultimasDezTaxas = new LinkedList<>();
+    private static final int CACHE_MAXIMO = 10;
+
+    @Autowired
+    public ExchangeClientWithFT(RestTemplate restTemplate, @Value("${app.services.exchange.url}") String exchangeServiceUrl) {
+        this.restTemplate = restTemplate;
+        this.exchangeServiceUrl = exchangeServiceUrl;
     }
+
+    public Double getExchangeRate() {
+        String url = exchangeServiceUrl + "/convert";
+
+        try {
+            Double taxa = restTemplate.getForObject(url, Double.class);
+
+            if (ultimasDezTaxas.size() >= CACHE_MAXIMO) {
+                ultimasDezTaxas.removeFirst();
+            }
+
+            ultimasDezTaxas.addLast(taxa);
+
+            return taxa;
+        } catch (Exception e) {
+            if (!ultimasDezTaxas.isEmpty()) {
+                double soma = 0.0;
+                for (Double taxaCache : ultimasDezTaxas) {
+                    soma += taxaCache;
+                }
+                return soma / ultimasDezTaxas.size();
+            }
+
+            throw new ExternalServiceException("Ocorreu um erro interno no serviço que fornece a taxa de conversão!");
+        }
+    } 
 }
